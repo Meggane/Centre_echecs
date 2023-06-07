fab4401d034c5ca333e996dbb0263a6e149a5b3b
Megane Berthelot <m-berthelot@hotmail.fr>
2023-06-07 15:27:19 +0200

    Added method to create the json file in order to retrieve the information of each player

--- players.py
+++ players.py
@@ -1,3 +1,4 @@
+import json
 import re
 import datetime
 
@@ -7,8 +8,16 @@ class Players:
         self.family_name = []
         self.first_name = []
         self.date_of_birth = []
+        self.list_of_all_player_numbers = []
 
-    def add_players(self, add_new_player="o"):
+    def add_players(self, add_new_player="o", player_number=0):
+        """Add players to the tournament with the control terminal
+
+        Manually enter family_name, first_name and date_of_birth
+
+        The number of each player is added automatically
+        """
+        dictionary_of_all_players = {}
         while add_new_player == "o":
             ask_family_name = input("Nom de famille : ")
             if re.search("[^a-zA-Z]", ask_family_name) or len(ask_family_name) < 2:
@@ -32,7 +41,25 @@ class Players:
                 print("La date de naissance n'est pas au format JJ/MM/AAAA. Veuillez réessayer")
                 break
 
+            player_number += 1
+            self.list_of_all_player_numbers.append(player_number)
+
+            for each_player_number, each_family_name, each_first_name, each_date_of_birth \
+                    in zip(self.list_of_all_player_numbers, self.family_name, self.first_name, self.date_of_birth):
+                dictionary_of_all_players.update({
+                    "Joueur numero " + str(each_player_number): {
+                        "Nom de famille": each_family_name,
+                        "Prenom": each_first_name,
+                        "Date de naissance": each_date_of_birth
+                    },
+                })
+
             next_player = input("Voulez-vous ajouter un autre joueur (o/n) : ")
             if next_player != "o":
                 break
-        return self.family_name, self.first_name, self.date_of_birth
+        return dictionary_of_all_players
+
+    def json_file_creation(self):
+        """Creation of the json file to retrieve the information of each player"""
+        with open("players.json", "w") as json_file:
+            json.dump(self.add_players(), json_file)
