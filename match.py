0c0c4e97c95a290d136520a7fd49bb5ea9dff8d3
Megane Berthelot <m-berthelot@hotmail.fr>
2023-06-21 10:19:00 +0200

    Modification of the match.py file : added the end_of_game_result function. It allows to retrieve the list of winning players and tied players at the end of each match.

--- match.py
+++ match.py
@@ -1,4 +1,5 @@
 import random
+import re
 import players
 
 
@@ -19,3 +20,18 @@ class Match:
         for each_player in range(0, len(mixed_list_players), 2):
             matches_list.append(mixed_list_players[each_player:each_player + 2])
         return matches_list
+
+    def end_of_game_result(self):
+        """Recovery of winning player and tied player numbers"""
+        ongoing_matches = self.random_player_selection()
+        list_of_winning_players = []
+        list_of_tied_players = []
+        for each_match in ongoing_matches:
+            ask_match_result = input(" contre ".join(each_match) + " | Y a t-il un gagnant ? (o/n)")
+            if ask_match_result == "o":
+                list_of_winning_players.append(int(input("Numéro du joueur gagnant : ")))
+            else:
+                tied_players = "".join(each_match)
+                number_of_tied_players = re.findall(r"[0-9]", tied_players)
+                list_of_tied_players.extend(number_of_tied_players)
+        return list_of_winning_players, list_of_tied_players
