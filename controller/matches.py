ab02ea224c2860167d90fabda1399da5ef6ef062
Megane Berthelot <m-berthelot@hotmail.fr>
2023-08-04 16:55:57 +0200

    Modification of the matches.py file: added the method to create tournament rounds

--- controller/matches.py
+++ controller/matches.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 import sys
 
 sys.path.append("..")
@@ -50,3 +51,38 @@ class Matches:
                     current_round_number: current_matches,
                 })
             model.Model().json_file_creation("matches.json", json_matches_file)
+
+    def creation_of_round(self):
+        """Creation of each round with the tournament and the corresponding round number, the list of matches with the
+        score of each player, the beginning and end of the date and time of the round. These are generated
+        automatically at the time of entry.
+        """
+        current_list_of_rounds = {}
+        json_matches_file = model.Model().json_file_playback("matches.json")
+        if os.path.isfile("../data/tournaments/rounds.json"):
+            json_rounds_file = model.Model().json_file_playback("rounds.json")
+            current_list_of_rounds.update(json_rounds_file)
+
+        current_start_date_and_hour = datetime.datetime.now()
+        tour_start_date = current_start_date_and_hour.strftime("%d/%m/%Y")
+        tour_start_time = current_start_date_and_hour.strftime("%H:%M:%S")
+
+        rounds_list = []
+        for round_number in json_matches_file:
+            rounds_list.append(round_number)
+
+        json_tournaments_file = model.Model().json_file_playback("tournaments.json")
+        list_of_tournament_numbers = []
+        for tournament_number in json_tournaments_file:
+            list_of_tournament_numbers.append(tournament_number)
+        for matches_of_each_round in json_matches_file.values():
+            current_list_of_rounds.update({
+                f"{list_of_tournament_numbers[-1]} / {rounds_list[-1]}": matches_of_each_round,
+                f"Infos du {list_of_tournament_numbers[-1]} / {rounds_list[-1]}": {
+                    "Date de debut": tour_start_date,
+                    "Heure de debut": tour_start_time,
+                    "Date de fin": "En cours",
+                    "Heure de fin": "En cours",
+                }
+            })
+        model.Model().json_file_creation("rounds.json", current_list_of_rounds)
